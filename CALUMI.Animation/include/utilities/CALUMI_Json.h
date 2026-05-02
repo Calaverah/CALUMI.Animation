9ef9023f7cdf6dbb6b225ce0448a7a8f18ff53e0
Cal Verah <Calaverahmedia@gmail.com>
2026-05-02 01:13:28 -0500

    json started

---
+++ CALUMI.Animation/include/utilities/CALUMI_Json.h
@@ -0,0 +1,109 @@
+//Copyright © 2025-2026 Calaverah. All rights reserved.
+//License: https://www.gnu.org/licenses/lgpl-3.0.html
+//Contact: Calaverahmedia@gmail.com
+
+// ReSharper disable CppNonExplicitConvertingConstructor
+#pragma once
+
+#include "CALUMI_Common.h"
+#include "CALUMI_Utilities.h"
+
+namespace CALUMI::Utilities
+{
+    class JsonObject;
+    class JsonArray;
+
+    class CALUMIANIMATION_API JsonValue
+    {
+    public:
+        enum class VarType
+        {
+            Char, ///< 1 Byte Character
+            String, ///< C Style String
+            Int8, ///< 1 Byte Signed Value
+            UInt8, ///< 1 Byte Unsigned Value
+            Int16, ///< 2 Byte Signed Value
+            UInt16, ///< 2 Byte Unsigned Value
+            Int32, ///< 4 Byte Signed Value
+            UInt32, ///< 4 Byte Unsigned Value
+            Int64, ///< 8 Byte Signed Value
+            UInt64, ///< 8 Byte Unsigned Value
+            Float, ///< 4 Byte Floating Point Value
+            Double, ///< 8 Byte Floating Point Value
+            Bool, ///< Boolean Value
+            JsonObject, ///< JsonObject as Value
+            JsonArray ///< JsonArray as Value
+        };
+
+        JsonValue();
+        JsonValue(bool value);
+        JsonValue(float value);
+        JsonValue(double value);
+        JsonValue(const char* value);
+        JsonValue(char value);
+        JsonValue(signed char value);
+        JsonValue(unsigned char value);
+        JsonValue(short value);
+        JsonValue(unsigned short value);
+        JsonValue(int value);
+        JsonValue(unsigned int value);
+        JsonValue(long long value);
+        JsonValue(unsigned long long value);
+        JsonValue(const JsonObject& value);
+        JsonValue(const JsonArray& value);
+        JsonValue(const JsonValue& input);
+        JsonValue(JsonValue&& input) noexcept;
+        ~JsonValue();
+
+        VarType varType() const;
+
+        bool toBool(bool defaultValue = false, bool* ok = nullptr) const;
+        float toFloat(float defaultValue = 0.0f, bool* ok = nullptr) const;
+        double toDouble(double defaultValue = 0.0f, bool* ok = nullptr) const;
+        const char* toString(bool* ok = nullptr) const;
+        char toChar(char defaultValue = 0, bool* ok = nullptr) const;
+        unsigned char toUInt8(unsigned char defaultValue = 0, bool* ok = nullptr) const;
+        signed char toInt8(signed char defaultValue = 0, bool* ok = nullptr) const;
+        unsigned short toUInt16(unsigned short defaultValue = 0, bool* ok = nullptr) const;
+        short toInt16(short defaultValue = 0, bool* ok = nullptr) const;
+        unsigned int toUInt(unsigned int defaultValue = 0, bool* ok = nullptr) const;
+        int toInt(int defaultValue = 0, bool* ok = nullptr) const;
+        unsigned long long toUInt64(unsigned long long defaultValue = 0, bool* ok = nullptr) const;
+        long long toInt64(long long defaultValue = 0, bool* ok = nullptr) const;
+        JsonObject toObject(bool* ok = nullptr) const;
+        JsonArray toArray(bool* ok = nullptr) const;
+
+        StringContainer serialize() const;
+
+        JsonValue& operator=(const JsonValue& input);
+        JsonValue& operator=(JsonValue&& input) noexcept;
+
+    private:
+        struct Impl;
+        Impl* pImpl;
+    };
+
+    VECTORDECF(JsonArray, JsonValue, friend class JsonObject; friend class JsonValue; );
+
+    class CALUMIANIMATION_API JsonObject
+    {
+
+    public:
+        JsonObject();
+        ~JsonObject();
+        JsonObject(const JsonObject& input);
+
+        JsonObject& operator=(const JsonObject& input);
+        JsonValue& operator[](const char* key) const;
+
+        bool contains(const char* key) const;
+        StringList keys() const;
+
+        StringContainer serialize(unsigned int indentOffset = 0, bool raw = false) const;
+
+    private:
+        struct Impl;
+        Impl* pImpl;
+    };
+
+}
---
+++ CALUMI.Animation/src/utilities/CALUMI_Json.cpp
@@ -0,0 +1,544 @@
+//Copyright © 2025-2026 Calaverah. All rights reserved.
+//License: https://www.gnu.org/licenses/lgpl-3.0.html
+//Contact: Calaverahmedia@gmail.com
+
+#include "utilities/CALUMI_Json.h"
+
+#include <format>
+#include <ranges>
+
+#include "internal/internalvectordef.h"
+
+#include <string>
+#include <typeinfo>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+
+namespace CALUMI::Utilities
+{
+    VECTORDEF(JsonArray, JsonValue);
+
+    struct JsonValue::Impl
+    {
+        std::variant<char, std::string,
+                     signed char, unsigned char,
+                     short, unsigned short,
+                     int, unsigned int,
+                     long long, unsigned long long,
+                     float, double,
+                     bool,
+                     JsonObject, JsonArray> m_value;
+    };
+
+    JsonValue::JsonValue() : pImpl(new Impl()) {}
+
+    JsonValue::JsonValue(bool value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(float value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(double value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(const char* value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(char value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(signed char value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(unsigned char value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(short value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(unsigned short value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(int value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(unsigned int value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(long long value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(unsigned long long value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(const JsonObject& value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(const JsonArray& value) : JsonValue()
+    {
+        pImpl->m_value = value;
+    }
+
+    JsonValue::JsonValue(const JsonValue& input) : JsonValue()
+    {
+        *this = input;
+    }
+
+    JsonValue::JsonValue(JsonValue&& input) noexcept : JsonValue()
+    {
+        *this = input;
+    }
+
+    JsonValue::~JsonValue()
+    {
+        if (pImpl)
+        {
+            delete pImpl;
+            pImpl = nullptr;
+        }
+    }
+
+    JsonValue::VarType JsonValue::varType() const
+    {
+        return static_cast<VarType>(pImpl->m_value.index());
+    }
+
+    bool JsonValue::toBool(const bool defaultValue, bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<bool>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+        return defaultValue;
+    }
+
+    float JsonValue::toFloat(const float defaultValue, bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<float>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return defaultValue;
+    }
+
+    double JsonValue::toDouble(const double defaultValue, bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<double>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+        return defaultValue;
+    }
+
+    const char* JsonValue::toString(bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<std::string>(pImpl->m_value).c_str();
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return "";
+    }
+
+    char JsonValue::toChar(const char defaultValue, bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<char>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return defaultValue;
+    }
+
+    unsigned char JsonValue::toUInt8(const unsigned char defaultValue, bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<unsigned char>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return defaultValue;
+    }
+
+    signed char JsonValue::toInt8(const signed char defaultValue, bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<signed char>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return defaultValue;
+    }
+
+    unsigned short JsonValue::toUInt16(const unsigned short defaultValue, bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<unsigned short>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return defaultValue;
+    }
+
+    short JsonValue::toInt16(const short defaultValue, bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<short>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return defaultValue;
+    }
+
+    unsigned int JsonValue::toUInt(const unsigned int defaultValue, bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<unsigned int>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return defaultValue;
+    }
+
+    int JsonValue::toInt(const int defaultValue, bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<int>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return defaultValue;
+    }
+
+    unsigned long long JsonValue::toUInt64(const unsigned long long defaultValue, bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<unsigned long long>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return defaultValue;
+    }
+
+    long long JsonValue::toInt64(const long long defaultValue, bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<long long>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return defaultValue;
+    }
+
+    JsonObject JsonValue::toObject(bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<JsonObject>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return JsonObject();
+    }
+
+    JsonArray JsonValue::toArray(bool* ok) const
+    {
+        if (ok)
+            *ok = true;
+
+        try
+        {
+            return std::get<JsonArray>(pImpl->m_value);
+        }
+        catch ( const std::bad_cast&) {}
+
+        if (ok)
+            *ok = false;
+
+        return JsonArray();
+    }
+
+    JsonValue& JsonValue::operator=(const JsonValue& input)
+    {
+        if (this != &input)
+        {
+            pImpl->m_value = input.pImpl->m_value;
+        }
+        return *this;
+    }
+
+    JsonValue& JsonValue::operator=(JsonValue&& input) noexcept
+    {
+        if (this != &input)
+        {
+            pImpl->m_value = input.pImpl->m_value;
+        }
+        return *this;
+    }
+
+    struct JsonObject::Impl
+    {
+        std::unordered_map<std::string, JsonValue> m_values;
+    };
+
+    JsonObject::JsonObject() : pImpl(new Impl()) {}
+
+    JsonObject::~JsonObject()
+    {
+        if (pImpl)
+        {
+            delete pImpl;
+            pImpl = nullptr;
+        }
+    }
+
+    JsonObject::JsonObject(const JsonObject& input) : JsonObject()
+    {
+        *this = input;
+    }
+
+    JsonObject& JsonObject::operator=(const JsonObject& input)
+    {
+        if (this != &input)
+        {
+            pImpl->m_values = input.pImpl->m_values;
+        }
+        return *this;
+    }
+
+    JsonValue& JsonObject::operator[](const char* key) const
+    {
+        return pImpl->m_values[key];
+    }
+
+    bool JsonObject::contains(const char* key) const
+    {
+        return pImpl->m_values.contains(key);
+    }
+
+    StringList JsonObject::keys() const
+    {
+        const StringList output;
+        output.reserve(pImpl->m_values.size());
+        for (const auto& key : pImpl->m_values | std::views::keys)
+        {
+            output.push_back(key.c_str());
+        }
+        return output;
+    }
+
+
+    StringContainer JsonObject::serialize(unsigned int indentOffset, const bool raw) const
+    {
+        const std::string indent = !raw ? "\t" : "";
+        std::string newline = !raw ? "\n" : "";
+
+        for (unsigned int i = 0; i < indentOffset; i++)
+        {
+            newline += indent;
+        }
+
+
+        std::string output = "{" + newline + indent;
+        uint64_t keyCount = 0;
+        for (const auto& [key, value] : pImpl->m_values)
+        {
+            keyCount++;
+            output += std::format(R"("{}": )", key);
+
+            switch (value.varType())
+            {
+            case JsonValue::VarType::Char:
+                output += std::format("{}", value.toChar()).c_str();
+                break;
+            case JsonValue::VarType::String:
+                output += std::format(R"("{}")", value.toString()).c_str();
+                break;
+            case JsonValue::VarType::Int8:
+                output += std::format("{}", value.toInt8()).c_str();
+                break;
+            case JsonValue::VarType::UInt8:
+                output += std::format("{}", value.toUInt8()).c_str();
+                break;
+            case JsonValue::VarType::Int16:
+                output += std::format("{}", value.toInt16()).c_str();
+                break;
+            case JsonValue::VarType::UInt16:
+                output += std::format("{}", value.toUInt16()).c_str();
+                break;
+            case JsonValue::VarType::Int32:
+                output += std::format("{}", value.toInt()).c_str();
+                break;
+            case JsonValue::VarType::UInt32:
+                output += std::format("{}", value.toUInt()).c_str();
+                break;
+            case JsonValue::VarType::Int64:
+                output += std::format("{}", value.toInt64()).c_str();
+                break;
+            case JsonValue::VarType::UInt64:
+                output += std::format("{}", value.toUInt64()).c_str();
+                break;
+            case JsonValue::VarType::Float:
+                output += std::format("{}", value.toFloat()).c_str();
+                break;
+            case JsonValue::VarType::Double:
+                output += std::format("{}", value.toDouble()).c_str();
+                break;
+            case JsonValue::VarType::Bool:
+                output += value.toBool() ? "true" : "false";
+                break;
+            case JsonValue::VarType::JsonObject:
+                output += value.toObject().serialize(indentOffset+2,raw).c_str();
+                break;
+            case JsonValue::VarType::JsonArray:
+                {
+                    output += newline + indent + indent + "[";
+
+                    for (const auto arrayCopy = value.toArray(); const auto& entry : arrayCopy.pImpl->vector)
+                    {
+                        output += newline + indent + indent + indent + entry.serialize(indent + 4, raw).c_str();
+                    }
+                    break;
+                }
+            default:
+                output += "null";
+                break;
+            }
+            if (keyCount < pImpl->m_values.size())
+                output += ",";
+        }
+
+        output += newline.c_str();
+        output += offset.c_str();
+        output += "}";
+
+        return output.c_str();
+    }
+}
